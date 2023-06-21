b476fd6a27722894ac7b66282bc0b752858aae74
kimphysicsman <kimphysicsman@gmail.com>
2023-06-21 13:33:57 +0900

    feat/ log 포멧 변경

--- api/log/views.py
+++ api/log/views.py
@@ -11,7 +11,11 @@ class HomeView(APIView):
 
     def get(self, request):
 
-        self.logger.debug("request home get method")
+        self.logger.debug({
+            "method": "get",
+            "user": "dongwoo",
+            "app": "test"
+        })
 
         return render(request, 'index.html')
 
@@ -19,34 +23,33 @@ class LogView(APIView):
     logger = logging.getLogger(__name__)
 
     def get(self, request):
-        client = boto3.client('logs')        
+        client = boto3.client('logs', region_name='ap-northeast-2')        
         log_group_name = 'test'
+        log_stream_name = 'test'
 
-        response = client.describe_log_streams(logGroupName=log_group_name)
+        log_events_response = client.get_log_events(
+                        logGroupName=log_group_name,
+                        logStreamName=log_stream_name
+                    )
 
-        # 모든 로그 스트림을 반복하면서 각각의 로그 이벤트를 가져옵니다.
-        for log_stream in response['logStreams']:
-            log_stream_name = log_stream['logStreamName']
-            print('------' , log_stream_name, '--------')
+        # log_events_response = client.filter_log_events(
+        #         logGroupName=log_group_name,
+        #         filterPattern='request'
+        #     )
 
-            # 각 로그 스트림의 로그 이벤트를 가져옵니다.
-            log_events_response = client.get_log_events(
-                logGroupName=log_group_name,
-                logStreamName=log_stream_name
-            )
 
-            # 각 로그 이벤트의 메시지를 출력합니다.
-            for log_event in log_events_response['events']:
-                print(log_event['message'])
+        # 각 로그 이벤트의 메시지를 출력합니다.
+        for log_event in log_events_response['events']:
+            print(log_event['message'])
 
         return Response({}, status=status.HTTP_200_OK)
 
     def post(self, request):
-        print({
-            "url": "log",
-            "method": "post",
-            })
         
-        self.logger.debug("request log post method")
+        self.logger.debug({
+            "method": "post",
+            "user": "dongwoo",
+            "app": "test"
+        })
 
         return Response({}, status=status.HTTP_200_OK)
