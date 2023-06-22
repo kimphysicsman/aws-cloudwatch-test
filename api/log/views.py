e354b0a1fc6effc1de49ad9a0d14a4a80257ae8f
kimphysicsman <kimphysicsman@gmail.com>
2023-06-22 16:50:55 +0900

    feat/ 테스트 데이터 생성 개선

--- api/log/views.py
+++ api/log/views.py
@@ -4,31 +4,47 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import json
-import logging
 import boto3
 import datetime 
 from faker import Faker
 
-class HomeView(APIView):
-    logger = logging.getLogger(__name__)
-
-    def get(self, request):
-
-        data = {
-            "method": "get",
-            "user": "dongwoo",
-            "app": "test"
+"""boto3를 통해 로그데이터를 CloudWatch에 저장하는 함수
+"""
+def put_log_data_list(data_list):
+    # 로그이벤트 리스트 생성
+    log_events = []
+    for data in data_list:
+        log_event = {
+            'timestamp': int(datetime.datetime.now().timestamp() * 1000),
+            'message': json.dumps(data)
         }
+        log_events.append(log_event)
 
-        self.logger.debug(json.dumps(data))
+    # CloudWatch 저장소 정보
+    log_group_name = 'test2'
+    log_stream_name = 'test2'
 
+    client = boto3.client('logs', region_name='ap-northeast-2')
+
+    response = client.put_log_events(
+        logGroupName=log_group_name,
+        logStreamName=log_stream_name,
+        logEvents=log_events
+    )
+
+    return response
+
+
+
+class HomeView(APIView):
+    def get(self, request):
         return render(request, 'index.html')
-    
+
     def post(self, request):
         fake = Faker("ko_KR")
 
-
-        for i in range(0, 15):
+        data_list = []
+        for i in range(0, 10000):
             mall_id = 'wendy' if  i % 2 == 0 else 'hani'
             shop_no = '1' if i % 3 == 0 else '2'
             user = fake.name()
@@ -50,72 +66,41 @@ class HomeView(APIView):
                 "value": value,
                 "is_mobile": is_mobile,                
             }
+            data_list.append(data)
 
-            # print(data)
-            self.logger.debug(json.dumps(data))
+        response = put_log_data_list(data_list)
 
 
-        return Response({}, status=status.HTTP_200_OK)
+        return Response({
+            "message": "done", 
+            }, status=status.HTTP_200_OK)
 
 class LogView(APIView):
-    logger = logging.getLogger(__name__)
-
     def get(self, request):
 
         start = datetime.datetime.now()
         start_timestamp = start.timestamp() * 1000
 
-        log_group_name = 'test'
-        log_stream_name = 'test'
+        log_group_name = 'test2'
+        log_stream_name = 'test2'
 
         client = boto3.client('logs', region_name='ap-northeast-2')
         log_events_response = client.filter_log_events(
                 logGroupName=log_group_name,
+                logStreamNames=[
+                    log_stream_name
+                ],
                 startTime=int(1687324392442),
                 endTime=int(start_timestamp),
-                limit=100,
+                limit=10000,
                 filterPattern='{ $.mall_id = wendy }'
-                # filterPattern='{ $.method = "post" }'
             )
 
         end = datetime.datetime.now()
         print("filter_log_events :", end - start)
 
-        # # 각 로그 이벤트의 메시지를 출력합니다.
-        # for log_event in log_events_response['events']:
-        #     print(json.loads(log_event['message']))
-
-
-        # log_events_response = client.get_log_events(
-        #                 logGroupName=log_group_name,
-        #                 logStreamName=log_stream_name
-        #             )
-        
-        # # 각 로그 이벤트의 메시지를 출력합니다.
-        # for event in log_events_response['events']:
-        #     message = event['message']
-        #     # 로그 메시지가 JSON 형태라고 가정하고 파싱
-        #     try:
-        #         log_data = json.loads(message)
-        #         print(log_data)
-        #     except json.JSONDecodeError:
-        #         pass
-
-        
-        # end_2 = datetime.datetime.now()
-        # print("log_events_response['events']", len(log_events_response['events']))
-        # print("get_log_events :", end_2 - end)
+        # 각 로그 이벤트의 메시지를 출력합니다.
+        for log_event in log_events_response['events']:
+            print(json.loads(log_event['message']))
 
         return Response({'response' : log_events_response}, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        
-        data = {
-            "method": "post",
-            "user": "dongwoo",
-            "app": "test"
-        }
-
-        self.logger.debug(json.dumps(data))
-
-        return Response({}, status=status.HTTP_200_OK)
