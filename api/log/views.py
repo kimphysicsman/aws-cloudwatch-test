58418116274008c01ededb911099f822d6368a07
kimphysicsman <kimphysicsman@gmail.com>
2023-06-22 09:53:23 +0900

    feat/ 로그 포멧 JSON으로 변경

--- api/log/views.py
+++ api/log/views.py
@@ -3,19 +3,23 @@ from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
+import json
 import logging
 import boto3
+import datetime 
 
 class HomeView(APIView):
     logger = logging.getLogger(__name__)
 
     def get(self, request):
 
-        self.logger.debug({
+        data = {
             "method": "get",
             "user": "dongwoo",
             "app": "test"
-        })
+        }
+
+        self.logger.debug(json.dumps(data))
 
         return render(request, 'index.html')
 
@@ -23,33 +27,44 @@ class LogView(APIView):
     logger = logging.getLogger(__name__)
 
     def get(self, request):
-        client = boto3.client('logs', region_name='ap-northeast-2')        
+
+        now = datetime.datetime.now()
+        timestamp = now.timestamp() * 1000
+
         log_group_name = 'test'
         log_stream_name = 'test'
 
-        log_events_response = client.get_log_events(
-                        logGroupName=log_group_name,
-                        logStreamName=log_stream_name
-                    )
+        client = boto3.client('logs', region_name='ap-northeast-2')
 
-        # log_events_response = client.filter_log_events(
-        #         logGroupName=log_group_name,
-        #         filterPattern='request'
-        #     )
+        # log_events_response = client.get_log_events(
+        #                 logGroupName=log_group_name,
+        #                 logStreamName=log_stream_name
+        #             )
+
+        log_events_response = client.filter_log_events(
+                logGroupName=log_group_name,
+                startTime=int(1687324392442),
+                endTime=int(timestamp),
+                limit=100,
+                # filterPattern='{ $.method = post }'
+                # filterPattern='{ $.method = "post" }'
+            )
 
 
         # 각 로그 이벤트의 메시지를 출력합니다.
-        for log_event in log_events_response['events']:
-            print(log_event['message'])
+        # for log_event in log_events_response['events']:
+        #     print(log_event['message'])
 
-        return Response({}, status=status.HTTP_200_OK)
+        return Response({'response' : log_events_response}, status=status.HTTP_200_OK)
 
     def post(self, request):
         
-        self.logger.debug({
+        data = {
             "method": "post",
             "user": "dongwoo",
             "app": "test"
-        })
+        }
+
+        self.logger.debug(json.dumps(data))
 
         return Response({}, status=status.HTTP_200_OK)
